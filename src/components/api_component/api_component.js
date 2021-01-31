40b73d2f081b64a59ca79d040f08debcf055b03b
Nick Tillinghast <nickt2@cox.net>
2021-01-31 10:14:06 -0700

    deleted .env

--- src/components/api_component/api_component.js
+++ src/components/api_component/api_component.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 
 // import axios from 'axios'
 
 const Api = () => {
+    const [person, setPerson] = useState(null)
+
+    useEffect(async () => {
+        const response = await fetch("https://api.randomuser.me/");
+        const data = await response.json();
+        const [item] = data.results;
+        setPerson(item);
+    }, [])
+
     return (
-        <div>hello world</div>
+
+        <div>hello world
+
+            {person && <div>{person.name.first}</div>}
+        </div>
     )
 }
 
