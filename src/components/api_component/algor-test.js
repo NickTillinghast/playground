50eb1fd1140c47f80a533c512be6c52a317b959c
Nick Tillinghast <nickt2@cox.net>
2021-05-05 10:02:11 -0600

    added recursive function

--- src/components/api_component/algor-test.js
+++ src/components/api_component/algor-test.js
@@ -9,3 +9,25 @@ function gcd(a, b) = {
 }
 
 console.log(gcd(20, 8));
+// recursive functions
+/*
+
+1. base case
+2. different input
+*/
+function countDown(num) {
+    if (num <= 0) {
+        console.log("all done");
+        return
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
+
+
+
+countDown(5);
+
+
+
